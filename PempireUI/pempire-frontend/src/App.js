603e9b2919711583a35f71a2314bbad4d1dfc37c
Natalie S <66453648+nsxcode@users.noreply.github.com>
2021-12-02 21:42:06 -0500

    Sign Up page is styled

--- PempireUI/pempire-frontend/src/App.js
+++ PempireUI/pempire-frontend/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import HomeScreen from "./components/HomeScreen/HomeScreen";
 import {GlobalStyle} from "./components/GlobalStylings";
 import LoginScreen from "./components/LoginScreen/LoginScreen";
+import SignUpScreen from "./components/SignUpScreen/SignUpScreen";
 
 function App() {
   return (
-    <LoginScreen/>
+    <SignUpScreen/>
   );
 }
 
---
+++ PempireUI/pempire-frontend/src/components/SignUpScreen/SignUpStylings.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CustomButton = styled.button`
+  transform: translateY(-570%);
+  font-family: 'ArcadeClassic', serif ;
+  color: white;
+  font-size: 55px;
+  margin-bottom: 2%;
+  width: 20%;
+  background: transparent;
+  border-radius: 10px;
+`

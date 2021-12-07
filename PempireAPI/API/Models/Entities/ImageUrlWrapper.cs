129758b4ef046b2fc50b7c3443cc8d7707eb40c1
Raaid <raaidsm@gmail.com>
2021-12-07 13:33:02 -0500

    Add wrapper for image url in Entity class

---
+++ PempireAPI/API/Models/Entities/ImageUrlWrapper.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.Entities
+{
+    public class ImageUrlWrapper
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; } = "";
+    }
+}

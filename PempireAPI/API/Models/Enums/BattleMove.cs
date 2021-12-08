b1ab67a568a7994a1e64d7aa38a5889b364cdc4a
Raaid Mahboob <raaidsm@gmail.com>
2021-12-08 18:25:41 -0500

    Edit names of BattleMoves and add the example Attacks to BattleActonFactory

--- PempireAPI/API/Models/Enums/BattleMove.cs
+++ PempireAPI/API/Models/Enums/BattleMove.cs
@@ -2,7 +2,8 @@ namespace API.Models.Enums
 {
     public enum BattleMove
     {
-        TestBA1, TestBA2, TestBA3, TestBA4, TestBA5, TestBA6, TestBA7, TestBA8,
-        TestSM1, TestSM2, TestSM3, TestSM4, TestSM5, TestSM6, TestSM7, TestSM8
+        NullBattleMove,
+        Att1, Att2, Att3, Att4,
+        SM1, SM2, SM3, SM4
     }
 }
--- PempireAPI/API/Models/Helpers/BattleActionFactory.cs
+++ PempireAPI/API/Models/Helpers/BattleActionFactory.cs
@@ -2,15 +2,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models.Enums;
 using API.Models.Helpers;
 
 namespace API.Models.Entities
 {
     public static class BattleActionFactory
     {
-        public static BattleAction GetBattleActionByName(string battleActionName)
+        private static Dictionary<BattleMove, BattleAction?> _allBattleActions;
+
+        static BattleActionFactory()
+        {
+            _allBattleActions = new Dictionary<BattleMove, BattleAction?>
+            {
+                {BattleMove.NullBattleMove, null},
+                {BattleMove.Att1, new BattleAction
+                {
+                    Name = "Violent Charge",
+                    Description = "A deadly charge. Deals 50 damage but deals 25 recoil to user.",
+                    BattleActionType = BattleActionType.Attack,
+                    Element = Element.Normal,
+                    EntityChanges = new EntityStateChanges(-25, 5, -50, 0),
+                    ManaCost = -5
+                }},
+                {BattleMove.Att2, new BattleAction
+                {
+                    Name = "Corrosive Punch",
+                    Description = "Does only 20 damage but builds 35 mana. Also lowers the foe's mana by 10.",
+                    BattleActionType = BattleActionType.Attack,
+                    Element = Element.Acid,
+                    EntityChanges = new EntityStateChanges(0, 35, -30, -10),
+                    ManaCost = -35
+                }},
+                {BattleMove.Att3, new BattleAction
+                {
+                    Name = "Fireball",
+                    Description = "Launches a fireball that deals 40 damage. Uses 25 mana.",
+                    BattleActionType = BattleActionType.Attack,
+                    Element = Element.Fire,
+                    EntityChanges = new EntityStateChanges(0, -25, -40, 0),
+                    ManaCost = 25
+                }},
+                {BattleMove.Att4, new BattleAction
+                {
+                    Name = "Full-Power Blast",
+                    Description = "An all-out assault. Deals 80 damage at the cost of 100 mana.",
+                    BattleActionType = BattleActionType.Attack,
+                    Element = Element.Normal,
+                    EntityChanges = new EntityStateChanges(0, -100, -80, 0),
+                    ManaCost = 100
+                }}
+            };
+        }
+
+        public static BattleAction GenerateBattleAction(BattleMove battleActionName)
         {
             //Take a string and return the information for the battle action of that name
+            //BattleMove in this method is the equivalent to `Consumable` in `ItemFactory.GetItemByName()`
             //TODO: Placeholder value
             return new BattleAction();
         }

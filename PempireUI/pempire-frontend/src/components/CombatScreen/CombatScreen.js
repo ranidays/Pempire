a1e8096709dd32602e592380ced0d9ef350d2a80
Raaid Mahboob <raaidsm@gmail.com>
2021-12-09 14:15:58 -0500

    Fix routing by adding to routes.js

--- PempireUI/pempire-frontend/src/components/CombatScreen/CombatScreen.js
+++ PempireUI/pempire-frontend/src/components/CombatScreen/CombatScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { Container } from "../GlobalStylings";
 
-const CombatScreen = () =>
-  <div>
-  </div>
+const CombatScreen = () => 
+  <Container>
+    <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/">Alright, let's head back</Link>
+  </Container>
 
 export default CombatScreen;

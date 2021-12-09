9199c804cb36e81ebcd9d4e690f4ae9d0f1a538d
AngelaVilladiego <villadia@sheridancollege.ca>
2021-12-08 19:18:24 -0500

    Item factory complete with items. Pass Consumable enum to GetItem and get corresponding item. Access ItemFactory.AllItems for list of all items.

--- PempireAPI/API/Controllers/ShopController.cs
+++ PempireAPI/API/Controllers/ShopController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using API.Models.Helpers;
+using System.Web.Http.Cors;
 
 namespace API.Controllers
 {
@@ -10,6 +12,10 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ShopController : ControllerBase
     {
-        
+        [HttpGet("test")]
+        public async Task<IActionResult> TestShop()
+        {        
+            return Ok(ItemFactory.AllItems);
+        }
     }
 }
--- PempireAPI/API/Models/Helpers/ItemFactory.cs
+++ PempireAPI/API/Models/Helpers/ItemFactory.cs
@@ -2,16 +2,162 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models.Entities;
+using API.Models.Enums;
 
 namespace API.Models.Helpers
 {
     public static class ItemFactory
     {
-        public static Item GetItemByName(string itemName)
+        public static Dictionary<Consumable, Item?> AllItems = new Dictionary<Consumable, Item?>
         {
-            //Take a string and return the information for the item of that name
-            //TODO: Placeholder value
-            return new Item();
+            {Consumable.NullUsable, null},
+
+            {Consumable.SmallHP, new Item{
+                Name = "Small Health Potion",
+                Description = "Heals for 15 HP",
+                ItemType = ItemType.Potion,
+                Element = Element.NullType,
+                EntityChanges = new EntityStateChanges(15, 0, 0, 0),
+                GoldCost = 10,
+                IconUrl = "health_small.png"
+            }},
+
+            {Consumable.MediumHP, new Item{
+                Name = "Medium Health Potion",
+                Description = "Heals for 30 HP",
+                ItemType = ItemType.Potion,
+                Element = Element.NullType,
+                EntityChanges = new EntityStateChanges(30, 0, 0, 0),
+                GoldCost = 18,
+                IconUrl = "health_medium.png"
+            }},
+
+            {Consumable.GreaterHP, new Item{
+                Name = "Greater Health Potion",
+                Description = "Heals for 60 HP",
+                ItemType = ItemType.Potion,
+                Element = Element.NullType,
+                EntityChanges = new EntityStateChanges(60, 0, 0, 0),
+                GoldCost = 30,
+                IconUrl = "health_large.png"
+            }},
+
+            {Consumable.SmallMP, new Item{
+                Name = "Small Mana Potion",
+                Description = "Restores 15 MP",
+                ItemType = ItemType.Potion,
+                Element = Element.NullType,
+                EntityChanges = new EntityStateChanges(0, 15, 0, 0),
+                GoldCost = 10,
+                IconUrl = "mana_small.png"
+            }},
+
+            {Consumable.MediumMP, new Item{
+                Name = "Medium Mana Potion",
+                Description = "Restores 30 MP",
+                ItemType = ItemType.Potion,
+                Element = Element.NullType,
+                EntityChanges = new EntityStateChanges(0, 30, 0, 0),
+                GoldCost = 18,
+                IconUrl = "mana_medium.png"
+            }},
+
+            {Consumable.GreaterMP, new Item{
+                Name = "Greater Mana Potion",
+                Description = "Restores 60 MP",
+                ItemType = ItemType.Potion,
+                Element = Element.NullType,
+                EntityChanges = new EntityStateChanges(0, 60, 0, 0),
+                GoldCost = 30,
+                IconUrl = "mana_large.png"
+            }},
+
+            {Consumable.WaterScroll, new Item{
+                Name = "Water Scroll",
+                Description = "Performs a water-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Water,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_water.png"
+            }},
+
+            {Consumable.FireScroll, new Item{
+                Name = "Fire Scroll",
+                Description = "Performs a fire-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Fire,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_fire.png"
+            }},
+
+            {Consumable.OilScroll, new Item{
+                Name = "Oil Scroll",
+                Description = "Performs an oil-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Oil,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_oil.png"
+            }},
+
+            {Consumable.GrassScroll, new Item{
+                Name = "Grass Scroll",
+                Description = "Performs a grass-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Grass,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_grass.png"
+            }},
+
+            {Consumable.RockScroll, new Item{
+                Name = "Rock Scroll",
+                Description = "Performs a rock-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Rock,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_rock.png"
+            }},
+
+            {Consumable.AcidScroll, new Item{
+                Name = "Acid Scroll",
+                Description = "Performs an acid-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Acid,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_acid.png"
+            }},
+
+            {Consumable.MetalScroll, new Item{
+                Name = "Metal Scroll",
+                Description = "Performs a metal-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Metal,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_metal.png"
+            }},
+
+            {Consumable.ElectricityScroll, new Item{
+                Name = "Electricity Scroll",
+                Description = "Performs an electricity-based attack at no mana cost",
+                ItemType = ItemType.Scroll,
+                Element = Element.Electricity,
+                EntityChanges = new EntityStateChanges(0, 0, -20, 0),
+                GoldCost = 50,
+                IconUrl = "scroll_electricity.png"
+            }},
+        };
+     
+        public static Item GetItem(Consumable itemType)
+        {
+            return AllItems[itemType];
         }
+
     }
 }

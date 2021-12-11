9d013980f4369465dd51646ba35304863a9c92ce
Raaid <raaidsm@gmail.com>
2021-12-10 22:26:34 -0500

    Add test code to CombatScreen.js

--- PempireUI/pempire-frontend/src/components/CombatScreen/CombatScreen.js
+++ PempireUI/pempire-frontend/src/components/CombatScreen/CombatScreen.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import { Container } from "../GlobalStylings";
 
-const CombatScreen = () => 
-  <Container>
-    <Link style={{ color: 'inherit', textDecoration: 'inherit'}} to="/">Alright, let's head back</Link>
+const CombatScreen = () => {
+  const [jwt, setJwt] = useState(null);
+
+  const handleClick = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ 
+        "Email" : "a@c.com",
+        "Password" : "Password1!"
+      })
+    };
+
+    fetch("http://localhost:5000/api/authentication/login", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      if (jwt === null) setJwt(data.token);
+      else alert(jwt);
+      console.log(`Successfully set ${data.token} as JWT!`);
+    });
+  }
+
+  return <Container>
+    <div
+    style={{ color: "inherit", textDecoration: "inherit", cursor: "pointer" }}
+    onClick={handleClick}>
+      For now, this logs you in</div>
   </Container>
+}
 
 export default CombatScreen;

3adfe0e159b7ccb30d48027466c1f89493b582b3
Raaid <raaidsm@gmail.com>
2021-12-11 19:16:48 -0500

    Add moves.js to help communicate moves to and from backend

---
+++ PempireUI/pempire-frontend/src/moves.js
@@ -0,0 +1,28 @@
+//This is an enum btw
+const MoveType = Object.freeze({
+    ATTACK: Symbol("attack"),
+    SPECIAL: Symbol("special"),
+    POTION: Symbol("potion"),
+    SCROLL: Symbol("scroll")
+});
+
+//This refers to attacks, specials, potions and scrolls
+class Move {
+    constructor(identifier = null, name = null, enumeration = null, moveType = null) {
+        this.identifier = identifier;
+        this.name = name;
+        this.enumeration = enumeration;
+        this.moveType = moveType;
+    }
+}
+
+const moves = [
+    new Move("Att1", "Violent Charge", 1, MoveType.ATTACK),
+    new Move("Att2", "Corrosive Punch", 2, MoveType.ATTACK),
+    new Move("Att3", "Fireball", 3, MoveType.ATTACK),
+    new Move("Att4", "Full-Power Blast", 4, MoveType.ATTACK),
+    new Move("SM1", "Corrosive Dart", 1, MoveType.SPECIAL),
+    new Move("SM2", "Bright Flash", 2, MoveType.SPECIAL),
+];
+
+export { MoveType, moves };

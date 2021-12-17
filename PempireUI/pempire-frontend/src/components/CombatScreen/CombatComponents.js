d0f07deca4700e90087a562f774ff3248c048966
Raaid <raaidsm@gmail.com>
2021-12-17 15:32:45 -0500

    Format CombatComponents better and remove unnecessary avatar class

--- PempireUI/pempire-frontend/src/components/CombatScreen/CombatComponents.js
+++ PempireUI/pempire-frontend/src/components/CombatScreen/CombatComponents.js
@@ -1,19 +1,48 @@
 import "./CombatStyles.css";
 
-const CombatOptions = (props) => <div className="combat-options">{props.children}</div>
+const CombatOptions = (props) => {
+  return <div
+  className="combat-options">
+    {props.children}
+  </div>
+}
 
-const MoveTypeDisplay = (props) => <div className="four-combat-options-display move-type-display">{props.children}</div>
+const MoveTypeDisplay = (props) => {
+  return <div
+  className="four-combat-options-display move-type-display">
+    {props.children}
+  </div>
+}
 
-const MoveDisplay = (props) => <div className="four-combat-options-display move-display">{props.children}</div>
+const MoveDisplay = (props) => {
+  return <div
+  className="four-combat-options-display move-display">
+    {props.children}
+  </div>
+}
 
-const CombatOptionButton = (props) => <div className="combat-option-button" onClick={props.onClick}>{props.children}</div>
+const CombatOptionButton = (props) => {
+  return <div
+  className="combat-option-button"
+  onClick={props.onClick}>
+    {props.children}
+  </div>
+}
 
-const BackButton = (props) => <p className="back-button">Back</p>
+const BackButton = (props) => {
+  return <p
+  className="back-button">
+    Back
+  </p>
+}
 
-const CombatProfile = (props) => <div className="combat-profile">
-  <p className="avatar">Avatar</p>
-  <p className="combat-info">Info</p>
-</div>
+const CombatProfile = (props) => {
+  return <div
+  className="combat-profile">
+    <p className="avatar">Avatar</p>
+    <p className="combat-info">Info</p>
+  </div>
+}
 
 
 export { CombatOptions, MoveTypeDisplay, MoveDisplay, CombatOptionButton, BackButton, CombatProfile };
